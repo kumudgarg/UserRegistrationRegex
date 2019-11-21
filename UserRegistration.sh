e152b7b1b62784aa98b0d5d278381557a57d1aaf
kumudgarg <kumud50garg@gmail.com>
2019-11-21 10:07:01 +0530

    last name regex pattern done

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,12 +1,31 @@
 #!/bin/bash -x
 shopt -s extglob
-echo "Enter your First Name"
-read firstName
-pat="^[A-Z][a-zA-Z]{2,}$"
-if [[ $firstName =~ $pat ]]
-then
-	echo valid;
-else
-	echo "invalid : first name should starts with uppercase letter with minimum 3 character" ;
-fi
+function firstNameRegex()
+{
+	echo "Enter your First Name"
+	read firstName
+	pat="^[A-Z][a-zA-Z]{2,}$"
+	if [[ $firstName =~ $pat ]]
+	then
+		echo "valid : first Name";
+	else
+		echo "invalid : first name should starts with uppercase letter with minimum 3 character" ;
+	fi
 
+}
+function lastNameRegex()
+{
+        echo "Enter your Last Name"
+        read lastName
+        pat="^[A-Z][a-zA-Z]{2,}$"
+        if [[ $lastName =~ $pat ]]
+        then
+                echo "valid : last Name";
+        else
+                echo "invalid : last name should starts with uppercase letter with minimum 3 character" ;
+        fi
+
+}
+
+#firstNameRegex
+lastNameRegex
